f38bbb424f62d1c2b4d88a9bf94108972357fb4b
FrazierMark <m.mark.frazier@gmail.com>
2023-10-23 13:23:57 -0700

    Initial commit

---
+++ BankAccount.py
@@ -0,0 +1,5 @@
+class BankAccount:
+    def __init__(self, owner_name, acct_num, acct_balance):
+        self.owner_name = owner_name
+        self.acct_num = acct_num
+        self.acct_balance = acct_balance
