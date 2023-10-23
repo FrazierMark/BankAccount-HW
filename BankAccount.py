e589bfaa3a608426dc11e7cdd426de3ce99eaa7e
FrazierMark <m.mark.frazier@gmail.com>
2023-10-23 13:37:59 -0700

    deposit method complete

--- BankAccount.py
+++ BankAccount.py
@@ -1,5 +1,29 @@
+import random
+
 class BankAccount:
-    def __init__(self, owner_name, acct_num, acct_balance):
+    def __init__(self, owner_name, account_num, account_balance = 0):
         self.owner_name = owner_name
-        self.acct_num = acct_num
-        self.acct_balance = acct_balance
+        self.acct_num = account_num
+        self.acct_balance = account_balance
+
+
+    def deposit(self, amount):
+        self.acct_balance += amount
+        print(f"Amount deposited: ${amount} new balance: ${self.acct_balance}")
+
+
+
+
+
+
+
+
+
+
+# Helper function to generate random unique account nuumber 
+def generate_account_num():
+    account = []
+    while True:
+        acct_num = random.randint(10000000, 99999999)
+        if acct_num not in accounts:
+            return acct_num
