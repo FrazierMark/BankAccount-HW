215ffe9df8bbda5b4bae36cc4ae0618781e4d801
FrazierMark <m.mark.frazier@gmail.com>
2023-10-23 13:52:00 -0700

    get_balance and withdraw complete

--- BankAccount.py
+++ BankAccount.py
@@ -12,18 +12,27 @@ class BankAccount:
         print(f"Amount deposited: ${amount} new balance: ${self.acct_balance}")
 
 
+    def withdraw(self, amount):
+        if self.acct_balance >= amount:
+            self.acct_balance -= amount
+            print(f"Amount withdrawn: ${amount}. New balance: {self.acct_balance}")
+        else:
+            self.acct_balance -= 10
+            print(f"Insufficient funds.")
 
-
-
-
-
+    def get_balance(self):
+        print(f"Welcome, {self.owner_name}. Your current account balance for account number: {self.acct_num} is ${self.acct_balance}")
 
 
 
 # Helper function to generate random unique account nuumber 
 def generate_account_num():
-    account = []
+    accounts = []
     while True:
         acct_num = random.randint(10000000, 99999999)
         if acct_num not in accounts:
             return acct_num
+
+new_account = generate_account_num()
+my_new_account = BankAccount('Mark', new_account, 23.43)
+my_new_account.get_balance()
